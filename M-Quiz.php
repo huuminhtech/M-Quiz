052b717e6ce8696751a7a9faf5f1bcb77906f7b6
Nguyễn Minh <huuminh.net@noreply.github.com>
2017-06-09 06:36:04 +0700

    Create Repository
    Create Repository

---
+++ M-Quiz.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: M-Quiz
+Plugin URI: https://huuminh.net/
+Description: M-Quiz is a source web application to create and manage online quiz, test, exam on your web server. Copyright &copy; 2017 <a href="https://huuminh.net/">HuuMinh Information Technologies</a>.
+Version: 1.0.0
+Author: Minh Nguyen
+Author URI: https://huuminh.net/
+License: GPLv2 or later
+Text Domain: huuminh
+*/
+
+
+
+?>
